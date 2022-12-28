3da1b794b2b69be63197441cfe5d6a61d336642c
devopslearn9145 <117148582+devopslearn9145@users.noreply.github.com>
2022-12-29 00:38:52 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -9,7 +9,7 @@ if ("${stageName}" == "Build")
       {
         sh "mvn clean sonar:sonar"
 }
-	else if("${stageName} == "Upload Into Nexus")
+	else if("${stageName}" == "Upload Into Nexus")
 	{ 
 	  sh "mvn clean deploy"
 	  }
